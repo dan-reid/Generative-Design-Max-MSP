79d2558fd220a1d022ae436eb013aedb57639b69
danreidxy <daniel.j.reid@hotmail.com>
2019-12-26 00:21:18 +0000

    Added lcg to math-utils

--- code/math-utils.js
+++ code/math-utils.js
@@ -47,6 +47,36 @@ exports.norm = function (n, start, stop) {
     return exports.map(n, start, stop, 0, 1);
 };
 
+var lcg = (function () {
+    // Set to values from http://en.wikipedia.org/wiki/Numerical_Recipes
+    // m is basically chosen to be large (as it is the max period)
+    // and for its relationships to a and c
+    var m = 4294967296,
+      // a - 1 should be divisible by m's prime factors
+      a = 1664525,
+      // c and m should be co-prime
+      c = 1013904223,
+      seed,
+      z;
+    return {
+      setSeed: function (val) {
+        // pick a random seed if val is undefined or null
+        // the >>> 0 casts the seed to an unsigned 32-bit integer
+        z = seed = (val == null ? Math.random() * m : val) >>> 0;
+      },
+      getSeed: function () {
+        return seed;
+      },
+      rand: function () {
+        // define the recurrence relationship
+        z = (a * z + c) % m;
+        // return a float in [0, 1)
+        // if z = m then z / m = 0 therefore (z % m) / m < 1 always
+        return z / m;
+      }
+    };
+  })();
+
 function hypot(x, y, z) {
     var length = arguments.length;
     var args = [];
