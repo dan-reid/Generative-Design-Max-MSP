bfbdf65e5ad93e0511257c4fc2434909477094b1
Daniel Reid <danielreid@WoebotAdmins-MacBook-Pro.local>
2020-02-17 22:04:44 +0000

    added color class to P_2_0

--- patchers/P_1_Hello_Color/P_1_1_2_Color_spectrum_in_a_circle/P_2.js
+++ patchers/P_1_Hello_Color/P_1_1_2_Color_spectrum_in_a_circle/P_2.js
@@ -1,27 +1,15 @@
 autowatch = 1;
 var { PClone } = require('PClone');
+var width = 500;
+var height = 500;
 var mg; // jit.mgraphics
 var outputmatrix; // the matrix to store each frame of animation
 var pc;
-var width = 500;
-var height = 500;
 
-var segmentCount;
+var segment_count;
 var radius;
-
-var maxhue;
-var maxsaturation;
-var maxbrightness;
 var saturation;
 var brightness;
-
-/*
-The color-wheel segements are arranged in the shape of a fan
-the vertices are computed from the sin and cos values of the
-corresponding angle. The Processing example uses the "TRIANGLE_FAN" mode of
-beginShape()but since we don't have that function in mgraphics
-a simpler method is to just draw a series of triangles.
-*/
 
 setup();
 
@@ -29,33 +17,30 @@ function setup() {
   mg = new JitterObject('jit.mgraphics', width, height);
   outputmatrix = new JitterMatrix(4, 'char', width, height);
   pc = new PClone();
-  segmentCount = 360;
+  segment_count = 360;
   radius = 220;
   maxhue = 360;
-  maxsaturation = width;
-  maxbrightness = height;
-  saturation = maxsaturation;
-  brightness = maxbrightness;
+  saturation = width;
+  brightness = height;
+  pc.color_mode('HSB', 360, width, height, 100);
 }
 
 function draw() {
   background(1, 1, 1, 1);
 
   mg.set_line_width(1);
-  var angleStep = 360 / segmentCount;
-  for (var angle = 0; angle < 360; angle += angleStep) {
+  var angle_step = 360 / segment_count;
+  for (var angle = 0; angle < 360; angle += angle_step) {
     var x1 = width / 2 + Math.cos(pc.radians(angle)) * radius;
     var y1 = height / 2 + Math.sin(pc.radians(angle)) * radius;
-    var x2 = width / 2 + Math.cos(pc.radians(angle + angleStep)) * radius;
-    var y2 = height / 2 + Math.sin(pc.radians(angle + angleStep)) * radius;
+    var x2 = width / 2 + Math.cos(pc.radians(angle + angle_step)) * radius;
+    var y2 = height / 2 + Math.sin(pc.radians(angle + angle_step)) * radius;
 
     // The example is the book uses HSB (same as HSV)
     // As far as I can tell you can't change the color mode for mgraphics
     // So we work in HSB and then simply convert to RGB before drawing
-    var colHSB = [angle / maxhue, saturation / maxsaturation, brightness / maxbrightness, 1];
-    // hsba_to_rgba is part of the PClone library.
-    var colRGB = pc.hsba_to_rgba(colHSB);
-    mg.set_source_rgba(colRGB);
+    var hsb = pc.color(angle, saturation, brightness, 100);
+    mg.set_source_rgba(hsb.normalize().to_rgb());
     triangle(x1, y1, x2, y2, width / 2, height / 2);
     mg.fill();
   }
@@ -64,28 +49,33 @@ function draw() {
 }
 
 function setSegmentCount(n) {
-  segmentCount = n;
+  segment_count = n;
 }
 
 function setSaturation(s) {
-  saturation = s * maxsaturation;
+  saturation = s * width;
 }
 
 function setBrightness(b) {
-  brightness = b * maxbrightness;
+  brightness = b * height;
 }
 
-// There are definitely simpler ways writing a triangle function
-// but this is the only way I could get the fill() command to work...
+/*
+The color-wheel segements are arranged in the shape of a fan
+the vertices are computed from the sin and cos values of the
+corresponding angle. The Processing example uses the "TRIANGLE_FAN" mode of
+beginShape() but since we don't have that function in mgraphics
+a simpler method is to just draw a series of triangles.
+*/
+
 function triangle(x1, y1, x2, y2, x3, y3) {
   var vec1 = [x2 - x3, y2 - y3];
   var vec2 = [x1 - x2, y1 - y2];
-  with (mg) {
-    move_to(x3, y3);
-    rel_line_to(vec1[0], vec1[1]);
-    rel_line_to(vec2[0], vec2[1]);
-    close_path();
-  }
+
+  mg.move_to(x3, y3);
+  mg.rel_line_to(vec1[0], vec1[1]);
+  mg.rel_line_to(vec2[0], vec2[1]);
+  mg.close_path();
 }
 
 function background(r, g, b, a) {
