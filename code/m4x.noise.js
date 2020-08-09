549d2ede7e6b37d91135ef1d2fbe19d0478142b2
Daniel Reid <danielreid@macbook-pro.home>
2020-08-09 10:44:07 +0100

    add noise_seed, add comments

--- code/m4x.noise.js
+++ code/m4x.noise.js
@@ -1,6 +1,4 @@
-function scaled_cosine(i) {
-	return 0.5 * (1.0 - Math.cos(i * Math.PI));
-}
+var { lcg } = require('util');
 
 var PERLIN_YWRAPB = 4;
 var PERLIN_YWRAP = 1 << PERLIN_YWRAPB;
@@ -13,6 +11,43 @@ var perlin_amp_falloff = 0.5; // 50% reduction/octave
 var perlin; // will be initialized lazily by noise()
 
 var noiseMethods = {
+	/**
+	 * Returns the Perlin noise value at specified coordinates. Perlin noise is
+	 * a random sequence generator producing a more naturally ordered, harmonic
+	 * succession of numbers compared to the standard random() function.
+	 * It was invented by Ken Perlin in the 1980s and been used since in
+	 * graphical applications to produce procedural textures, natural motion,
+	 * shapes, terrains etc.
+	 *
+	 * The main difference to the random() function is that Perlin noise is defined in an infinite
+	 * n-dimensional space where each pair of coordinates corresponds to a
+	 * fixed semi-random value. noise() can compute 1D, 2D and 3D noise,
+	 * depending on the number of coordinates given. The resulting value will
+	 * always be between 0.0 and 1.0. The noise value can be animated by moving
+	 * through the noise space as demonstrated in the example above. The 2nd
+	 * and 3rd dimension can also be interpreted as time.
+	 *
+	 * The actual noise is structured similar to an audio signal, in respect to the
+	 * function's use of frequencies. Similar to the concept of harmonics in
+	 * physics, perlin noise is computed over several octaves which are added
+	 * together for the final result.
+	 *
+	 * Another way to adjust the character of the resulting sequence is the scale of the input
+	 * coordinates. As the function works within an infinite space the value of
+	 * the coordinates doesn't matter as such, only the distance between
+	 * successive coordinates does (eg. when using noise() within a
+	 * loop). As a general rule the smaller the difference between coordinates,
+	 * the smoother the resulting noise sequence will be. Steps of 0.005-0.03
+	 * work best for most applications, but this will differ depending on use.
+	 *
+	 * @method noise
+	 * @param  {Number} x   x-coordinate in noise space
+	 * @param  {Number} [y] y-coordinate in noise space
+	 * @param  {Number} [z] z-coordinate in noise space
+	 * @return {Number}     Perlin noise value (between 0 and 1) at specified
+	 *                      coordinates
+	 */
+
 	noise: function (x, y, z) {
 		y = y || 0;
 		z = z || 0;
@@ -110,6 +145,10 @@ var noiseMethods = {
 	 * result in greater than 1.0 values returned by noise()
 	 * By changing these parameters, the signal created by the noise()
 	 * function can be adapted to fit very specific needs and characteristics.
+	 *
+	 * @method noise_detail
+	 * @param {Number} lod 			number of octaves to be used by the noise
+	 * @param {Number} falloff 	falloff factor for each octave
 	 */
 	noise_detail: function (lod, falloff) {
 		if (lod > 0) {
@@ -119,6 +158,28 @@ var noiseMethods = {
 			perlin_amp_falloff = falloff;
 		}
 	},
+
+	/**
+	 * Sets the seed value for noise(). By default, noise()
+	 * produces different results each time the program is run. Set the
+	 * value parameter to a constant to return the same pseudo-random
+	 * numbers each time the software is run.
+	 *
+	 * @method noise_seed
+	 * @param {Number} seed   the seed value
+	 */
+
+	noise_seed: function (seed) {
+		lcg.setSeed(seed);
+		perlin = [];
+		for (let i = 0; i < PERLIN_SIZE + 1; i++) {
+			perlin[i] = lcg.rand();
+		}
+	},
 };
+
+function scaled_cosine(i) {
+	return 0.5 * (1.0 - Math.cos(i * Math.PI));
+}
 
 exports.noise = noiseMethods;
